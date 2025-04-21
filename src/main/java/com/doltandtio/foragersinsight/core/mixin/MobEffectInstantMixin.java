ab9dbfefc773072b1b76e7309c3f338c2e0f5307
tiomadre <bd.pr.96@gmail.com>
2025-04-20 23:48:15 -0400

    Renamed, mod is now Foragers Insight

---
+++ src/main/java/com/doltandtio/foragersinsight/core/mixin/MobEffectInstantMixin.java
@@ -0,0 +1,8 @@
+package com.doltandtio.foragersinsight.core.mixin;
+
+import net.minecraft.world.effect.MobEffectInstance;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MobEffectInstance.class)
+public class MobEffectInstantMixin {
+}
--- src/main/java/com/doltandtio/naturesdelight/core/mixin/MobEffectInstantMixin.java
+++
@@ -1,8 +0,0 @@
-package com.doltandtio.naturesdelight.core.mixin;
-
-import net.minecraft.world.effect.MobEffectInstance;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MobEffectInstance.class)
-public class MobEffectInstantMixin {
-}
