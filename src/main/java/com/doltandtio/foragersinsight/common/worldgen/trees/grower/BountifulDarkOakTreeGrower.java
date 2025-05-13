5121f8eaae19f1204a1a1c1338fdd9cbc83da103
tiomadre <bd.pr.96@gmail.com>
2025-05-13 18:58:11 -0400

    Fixed Acorn Trees, players can now grow them by placing sapling in a 2x2 Block

--- src/main/java/com/doltandtio/foragersinsight/common/worldgen/trees/grower/BountifulDarkOakTreeGrower.java
+++ src/main/java/com/doltandtio/foragersinsight/common/worldgen/trees/grower/BountifulDarkOakTreeGrower.java
@@ -3,21 +3,14 @@ package com.doltandtio.foragersinsight.common.worldgen.trees.grower;
 import com.doltandtio.foragersinsight.common.worldgen.FIConfiguredFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.grower.AbstractMegaTreeGrower;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class BountifulDarkOakTreeGrower extends AbstractMegaTreeGrower {
+public class BountifulDarkOakTreeGrower extends AbstractTreeGrower {
     @Nullable
     @Override
-    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredMegaFeature(@NotNull RandomSource rand) {
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource pRandom, boolean pHasFlowers) {
         return FIConfiguredFeatures.ACORN_TREE_KEY;
-    }
-
-    @Nullable
-    @Override
-    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource rand, boolean pHasFlowers) {
-        return null;
     }
 }
--- src/main/java/com/doltandtio/foragersinsight/core/registry/FIFoliagePlacerType.java
+++ src/main/java/com/doltandtio/foragersinsight/core/registry/FIFoliagePlacerType.java
@@ -1,5 +1,6 @@
 package com.doltandtio.foragersinsight.core.registry;
 
+import com.doltandtio.foragersinsight.common.worldgen.trees.foliage.AcornTreeFoliagePlacer;
 import com.doltandtio.foragersinsight.common.worldgen.trees.foliage.AppleTreeFoliagePlacer;
 import com.doltandtio.foragersinsight.core.ForagersInsight;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
@@ -14,4 +15,8 @@ public class FIFoliagePlacerType {
     public static final RegistryObject<FoliagePlacerType<AppleTreeFoliagePlacer>> APPLE_FOLIAGE_PLACER =
             FOLIAGE_PLACER_TYPE.register("apple_tree_foliage_placer", () ->
                     new FoliagePlacerType<>(AppleTreeFoliagePlacer.CODEC));
+
+    public static final RegistryObject<FoliagePlacerType<AcornTreeFoliagePlacer>> ACORN_FOLIAGE_PLACER =
+            FOLIAGE_PLACER_TYPE.register("acorn_tree_foliage_placer", () ->
+                    new FoliagePlacerType<>(AcornTreeFoliagePlacer.CODEC));
 }
