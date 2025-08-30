39fb50f99c2958c6aff8693c1037129e55de0aab
tiomadre <bd.pr.96@gmail.com>
2025-08-30 09:54:55 -0400

    fix'd poppy n dandelion being funky, also bone meal now only increases these crops by 1 lvl instead of 2.

--- src/main/java/com/doltandtio/foragersinsight/common/block/DandelionBushBlock.java
+++ src/main/java/com/doltandtio/foragersinsight/common/block/DandelionBushBlock.java
@@ -5,14 +5,23 @@ import com.doltandtio.foragersinsight.core.registry.FIItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.NotNull;
 
 public class DandelionBushBlock extends CropBlock implements BonemealableBlock {
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_4;
+    public static final int MAX_AGE = 4;
+
     public DandelionBushBlock(Properties props) {
         super(props);
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0));
     }
 
     @Override
@@ -23,5 +32,24 @@ public class DandelionBushBlock extends CropBlock implements BonemealableBlock {
     @Override
     protected @NotNull ItemLike getBaseSeedId() {
         return FIItems.DANDELION_ROOT.get();
+    }
+
+    @Override
+    protected @NotNull IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+    @Override
+    protected int getBonemealAgeIncrease(@NotNull Level level) {
+        return 1;
     }
 }
--- src/main/java/com/doltandtio/foragersinsight/common/block/PoppyBushBlock.java
+++ src/main/java/com/doltandtio/foragersinsight/common/block/PoppyBushBlock.java
@@ -5,14 +5,23 @@ import com.doltandtio.foragersinsight.core.registry.FIItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import org.jetbrains.annotations.NotNull;
 
 public class PoppyBushBlock extends CropBlock implements BonemealableBlock {
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_4;
+    public static final int MAX_AGE = 4;
+
     public PoppyBushBlock(Properties pProperties) {
         super(pProperties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(AGE, 0));
     }
 
     @Override
@@ -25,4 +34,22 @@ public class PoppyBushBlock extends CropBlock implements BonemealableBlock {
         return FIItems.POPPY_SEEDS.get();
     }
 
+    @Override
+    protected @NotNull IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(AGE);
+    }
+    @Override
+    protected int getBonemealAgeIncrease(@NotNull Level level) {
+        return 1;
+    }
 }
