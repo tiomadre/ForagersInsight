96b8040911fc34d7eb53b1260f7d0dda0d30a045
tiomadre <bd.pr.96@gmail.com>
2025-09-20 09:42:26 -0400

    fixes to bowl ingredients, seed milk bucket & bottle: stack sizes, disappearing items, no return on bowl/bottles

--- src/main/java/com/doltandtio/foragersinsight/common/item/MilkBucketItem.java
+++ src/main/java/com/doltandtio/foragersinsight/common/item/MilkBucketItem.java
@@ -2,43 +2,48 @@ package com.doltandtio.foragersinsight.common.item;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.NotNull;
 
 public class MilkBucketItem extends Item {
     private static final int DRINK_DURATION = 32;
-    public MilkBucketItem (Properties settings) {
+
+    public MilkBucketItem(Properties settings) {
         super(settings);
     }
+
     @Override
-    public int getUseDuration(ItemStack stack) {
+    public int getUseDuration(@NotNull ItemStack stack) {
         return DRINK_DURATION;
     }
+
     @Override
-    public UseAnim getUseAnimation(ItemStack stack) {
+    public @NotNull UseAnim getUseAnimation(@NotNull ItemStack stack) {
         return UseAnim.DRINK;
     }
+
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+    public @NotNull InteractionResultHolder<ItemStack> use(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
         return ItemUtils.startUsingInstantly(level, player, hand);
     }
+
     @Override
-    public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
+    public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, Level level, @NotNull LivingEntity entity) {
         if (!level.isClientSide) {
             entity.removeAllEffects();
         }
-        if (entity instanceof Player player) {
 
-            if (!player.getAbilities().instabuild) {
-                return new ItemStack(Items.BUCKET);
-            }
+        if (entity instanceof Player player) {
+            return ItemUtils.createFilledResult(stack, player, Items.BUCKET.getDefaultInstance());
         }
-        return super.finishUsingItem(stack, level, entity);
+
+        return stack;
     }
 }
